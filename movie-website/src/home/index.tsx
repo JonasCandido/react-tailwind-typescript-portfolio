31d3414356b449e1c3473c3d195c3c9abb25d2d6
JonasCandido <jonasdcandido210@gmail.com>
2025-09-28 21:36:02 -0300

    Implement carousel

--- movie-website/src/home/index.tsx
+++ movie-website/src/home/index.tsx
@@ -3,7 +3,14 @@ import { ItensListRow } from "../itenslistrow";
 const Home = () => { 
   return (
     <main>
-      <ItensListRow />
+      <section>
+        <ItensListRow genre_id={12} row_title={"Experience Incredible Journeys"} />
+        <ItensListRow genre_id={28} row_title={"Feel the Beat of Your Heart"} />
+        <ItensListRow genre_id={16} row_title={"Worlds in Other Dimension"} />
+        <ItensListRow genre_id={80} row_title={"Thug Life"}/>
+        <ItensListRow genre_id={14} row_title={"Embrace Your Imagination"}/>
+        <ItensListRow genre_id={878} row_title={"Can You Imagine Science?"} />
+      </section>
     </main>
   );
 };
--- movie-website/src/itenslistrow/index.tsx
+++ movie-website/src/itenslistrow/index.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useReducer } from "react";
 import axios from 'axios';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 const moviesFetchInit = 'MOVIES_FETCH_INIT';
 const moviesFetchSuccess = 'MOVIES_FETCH_SUCCESS';
@@ -7,17 +12,27 @@ const moviesFetchFailure = 'MOVIES_FETCH_FAILURE';
 const api_key = "b6fc758812b079a8170bcb081bb7da04";
 const url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres=";
 
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+};
+
 interface MoviesState {
   data: any[];
   isLoading: boolean;
   isError: boolean;
-}
+};
 
 interface MoviesAction {
   type: string;
   payload?: any;
-}
+};
 
+interface ItensListRowProps {
+  genre_id: number;
+  row_title: string;
+};
 
 const moviesReducer = (state: MoviesState, action: MoviesAction) => {
     switch(action.type){
@@ -32,11 +47,10 @@ const moviesReducer = (state: MoviesState, action: MoviesAction) => {
     };
 };
 
-const ItensListRow = () => {
+const ItensListRow = ({genre_id, row_title}: ItensListRowProps) => {
 
   const [movies, dispatchMovies] = useReducer(moviesReducer,{data:[],isLoading:false,isError:false});
-  const genreId = 12;
-  const url_to_fetch = `${url}${genreId}&api_key=${api_key}`;
+  const url_to_fetch = `${url}${genre_id}&api_key=${api_key}`;
 
   useEffect(() => {
     dispatchMovies({ type: moviesFetchInit });
@@ -55,8 +69,52 @@ const ItensListRow = () => {
   }, []);
   
   return (
-    <article>
-      
+    <article className="my-10 ml-10">
+      <h2 className="text-3xl my-5">{row_title}</h2>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={7}
+        breakpoints={{
+          640: {
+            slidesPerView: 3, // até 640px de largura (mobile), mostra 2 slides
+            spaceBetween: 5,
+          },
+          768: {
+            slidesPerView: 4, // tablets
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 7, // desktop menor
+            spaceBetween: 10,
+          },
+        }}
+        navigation={{
+          nextEl: ".custom-next",
+          prevEl: ".custom-prev",
+        }}
+        modules={[Navigation]}
+        className="group relative"
+      >
+        {movies.data.map((movie: Movie) => (
+          <SwiperSlide key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={movie.title}
+              className="rounded-lg"
+            />
+            <p className="text-lg">{movie.title}</p>
+          </SwiperSlide>
+        ))}
+
+
+        <button className="custom-prev absolute left-0 top-1/2 -translate-y-1/2 bg-black/50 text-white p-2 opacity-0 group-hover:opacity-100 transition-opacity z-50">
+          <ChevronLeftIcon className="w-8 h-8" />
+        </button>
+        <button className="custom-next absolute right-0 top-1/2 -translate-y-1/2 bg-black/50 text-white p-2 opacity-0 group-hover:opacity-100 transition-opacity z-50">
+          <ChevronRightIcon className="w-8 h-8" />
+        </button>
+      </Swiper>
+
     </article>
   );
 };
