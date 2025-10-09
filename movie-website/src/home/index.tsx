911849f002eae824ac58dc86e7ce135c328e5ca1
JonasCandido <jonasdcandido210@gmail.com>
2025-10-09 10:18:59 -0300

    Move some tags from index.html to Home component
    Multiple SEO optimize tags were being rendered.
    Only page's tags now.

--- movie-website/src/home/index.tsx
+++ movie-website/src/home/index.tsx
@@ -4,6 +4,10 @@ const Home = () => {
   return (
     <main>
       <section>
+        <meta name="description" content="A website to watch movies online." />
+        <meta property="og:title" content="JWMovies" />
+        <meta property="og:title" content="JWMovies" />
+        <title>JWMovies | Watch Movies Online</title>
         <ItensListRow genre_id={12} row_title={"Experience Incredible Journeys"} />
         <ItensListRow genre_id={28} row_title={"Feel the Beat of Your Heart"} />
         <ItensListRow genre_id={16} row_title={"Worlds in Other Dimension"} />
